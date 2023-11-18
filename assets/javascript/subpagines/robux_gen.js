9d2b7b8e4cb294e78aac1d92f2782e44d200c2d9
amatgil <amatgilvinyes@gmail.com>
2023-11-18 01:00:01 +0100

    Fixed stickbug

--- assets/javascript/subpagines/robux_gen.js
+++ assets/javascript/subpagines/robux_gen.js
@@ -11,24 +11,28 @@ var i = 0;
 function progress_bar() {
   console.log("Bar-ing");
   if (i == 0) {
-    i = 1;
-    var elem = document.getElementById("virus_progress_bar");
-    var width = 1;
-    var id = setInterval(frame, 10);
-    function frame() {
-      if (width >= 100) {
-	      var bug = document.getElementById("stick_bug");
+	i = 1;
+	var elem = document.getElementById("virus_progress_bar");
+	var width = 1;
+	var id = setInterval(frame, 10);
+	function frame() {
+	  if (width >= 100) {
+		  var bug = document.getElementById("stick_bug");
+		  document.getElementById("stick_bug").src = "/assets/media/StickBug.webm";
 		  bug.style.display = "block";
-        clearInterval(id);
-        i = 0;
-      } else {
-        width += bell(i)/i/10;
-        elem.style.width = width + "%";
-      }
+		  clearInterval(id);
+		  i = 0;
+	  } else {
+	    i++;
+		console.log(width);
+		width = f(i/100)*100;
+		elem.style.width = width + "%";
+	  }
     }
   }
 } 
 
-function bell(x) {
-	return Math.exp(-x*x);
+function f(x) {
+	//return /*3*(x - x*x*x/3)/2*/;
+	return 2*Math.pow(x, 3/2)/3/10;
 }
