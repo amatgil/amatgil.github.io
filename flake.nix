d2f50557427bf97f9308777b6d267137079b359b
amatgil <amatgilvinyes@gmail.com>
2024-11-15 18:14:07 +0100

    Update nix setup

--- flake.nix
+++ flake.nix
@@ -1,26 +1,18 @@
 {
-  description = "wasm-pack setup for s-ic-mulador";
-
+  description = "Pàgina web d'Amat Gil";
   inputs = {
-    nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
   };
-
-  outputs = { self, nixpkgs }:
-    let system = "x86_64-linux";
-    in {
-      devShell.${system} = let
-        pkgs = import nixpkgs { inherit system; };
-      in (({ pkgs, ... }:
-        pkgs.mkShell {
-          buildInputs = with pkgs; [
-            jekyll 
-            rubyPackages.webrick
-            rubyPackages.jekyll-feed
-            rubyPackages.jekyll-redirect-from
-            bundler
-          ];
-
-          shellHook = "";
-        }) { pkgs = pkgs; });
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgs = (import nixpkgs { system = "x86_64-linux"; });
+    in
+    {
+      devShells = forAllSystems (system: {
+        default = pkgs.callPackage ./shell.nix { inherit pkgs; };
+      });
     };
 }
---
+++ shell.nix
@@ -0,0 +1,23 @@
+{
+  pkgs ? import <nixpkgs> { },
+  lib,
+}:
+let
+  packages = with pkgs; [
+    jekyll 
+    rubyPackages.webrick
+    rubyPackages.jekyll-feed
+    rubyPackages.jekyll-redirect-from
+    bundler
+  ];
+in
+pkgs.mkShell {
+  # Get dependencies from the main package
+  inputsFrom = [ (pkgs.callPackage ./default.nix { }) ];
+  nativeBuildInputs = packages;
+  buildInputs = packages;
+  env = {
+    LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+    LD_LIBRARY_PATH = "${lib.makeLibraryPath packages}";
+  };
+}
