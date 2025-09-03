d679a687782781ebfecaf6b5fe6b8c00ab66e1df
amatgil <amatgilvinyes@gmail.com>
2025-09-03 07:29:35 +0200

    Add a0b1c2

---
+++ subpages/utils/a0b1c2/a0b1c2.js
@@ -0,0 +1,52 @@
+
+// You can tell which parts i copied from the binary one because they don't have
+// semicolons lmao. Too much rust
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+// Error handling? Pffft lmao
+function toChar(input) {
+    return String.fromCharCode('a'.charCodeAt(0) + input);
+}
+function fromChar(input) {
+    return input.toLowerCase().charCodeAt(0) - 'a'.charCodeAt(0);
+}
+
+function isInputCorrect(current, input) {
+    // current is char, input is number
+    return fromChar(current) === input;
+}
+
+function inputhandler() {
+    entrada.addEventListener("keyup", ({key}) => {
+        if (key === "Enter") {
+            if (entrada.value == "") {
+                console.log("no")
+                return;
+            } 
+            if (isInputCorrect(challenge.innerText, Number(entrada.value))) {
+                feedback.innerHTML = 'Correcte! '.concat(challenge.innerText).concat('←').concat(entrada.value)
+                feedback.style.color = "#048A47"
+                challenge.innerText = toChar(getRandomInt(26))
+            } else {
+                feedback.innerHTML = "Incorrecte :("
+                feedback.style.color = "#DA6573"
+            }
+            entrada.value = null;
+        }
+    })
+}
+
+
+function putintothething() {
+    console.log(entrada.value);
+    console.log(challenge.innerText);
+    entrada.value = fromChar(challenge.innerText).toString();
+}
+
+
+window.addEventListener('load', inputhandler)
+window.addEventListener('load', () => skipbutton.addEventListener("click", putintothething))
+
+
